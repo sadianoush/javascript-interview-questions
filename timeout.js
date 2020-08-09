4411061146272add7bd6111bcd19e885623c585c
sadianoush <67622813+sadianoush@users.noreply.github.com>
2020-08-09 15:01:58 +0600

    JS question answers Done

---
+++ timeout.js
@@ -0,0 +1,22 @@
+function doSomething(){
+    console.log(3333);
+}
+console.log(2222);
+
+// setTimeout(function(){
+//     console.log('lazy and waiting')
+// }, 4000)
+ 
+
+// setTimeout(() => {
+//     console.log('see u later.')
+//  }, 4000)
+
+setInterval(function(){
+    console.log('doing it again') 
+}, 3000);
+
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
